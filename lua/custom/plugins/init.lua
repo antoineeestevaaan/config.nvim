fdfea7eebc561ed885e0fa7c66405e679c43ef97
amtoine <stevan.antoine@gmail.com>
2024-03-15 19:26:43 +0100

    refactor lualine

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,19 +5,6 @@ return {
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
 
   {
     -- Add indentation guides even on blank lines
