db580ab283603b48b1bceb0eb94b414963c452ba
amtoine <stevan.antoine@gmail.com>
2024-03-15 19:27:42 +0100

    refactor indent-blankline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,16 +5,6 @@ return {
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
-
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
