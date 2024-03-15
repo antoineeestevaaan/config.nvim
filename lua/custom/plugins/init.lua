39b86c896e387a5146fe43a599b3226460361f18
amtoine <stevan.antoine@gmail.com>
2024-03-15 19:33:21 +0100

    refactor aerial

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,24 +22,6 @@ return {
     end
   },
 
-  {
-    'stevearc/aerial.nvim',
-    opts = {},
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-      require('aerial').setup({
-        on_attach = function(bufnr)
-          vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-          vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
-        end
-      })
-      vim.keymap.set('n', '<leader>at', ':AerialToggle!<CR>')
-    end
-  },
-
   -- Git
   {
     "tpope/vim-fugitive",
