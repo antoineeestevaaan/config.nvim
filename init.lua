44e080602525d1563196ef5150091fed16647be0
amtoine <stevan.antoine@gmail.com>
2024-03-15 19:11:42 +0100

    format main init.lua

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({{ import = 'custom.plugins' }}, {})
+require('lazy').setup({ { import = 'custom.plugins' } }, {})
 
 pcall(require, 'custom/options')
 pcall(require, 'custom/map')
