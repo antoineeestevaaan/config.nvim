366784db6ef3ebb03bd93f6812aee18dad40e4de
amtoine <stevan.antoine@gmail.com>
2024-03-15 19:32:31 +0100

    refactor latex plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -23,23 +23,6 @@ return {
   },
 
   {
-    "nvim-telescope/telescope-bibtex.nvim",
-    requires = {
-      { 'nvim-telescope/telescope.nvim' },
-    },
-    config = function()
-      local telescope = require("telescope")
-
-      telescope.load_extension("bibtex")
-
-      vim.keymap.set("n", "<leader>bbt", function() telescope.extensions.bibtex.bibtex {} end,
-        { silent = true, desc = "propose [b]i[bt]ex references for copy" })
-    end,
-  },
-
-  { "lervag/vimtex" },
-
-  {
     'stevearc/aerial.nvim',
     opts = {},
     dependencies = {
