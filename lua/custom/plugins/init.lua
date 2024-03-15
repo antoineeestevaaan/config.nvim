189efa97dde8dffab69afdd600d6e4a90aa5e684
amtoine <stevan.antoine@gmail.com>
2024-03-15 19:39:42 +0100

    refactor last one

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,18 +6,4 @@ return {
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
-  {
-    'rmagatti/goto-preview',
-    config = function()
-      local goto_preview = require('goto-preview')
-      goto_preview.setup {}
-
-      vim.keymap.set("n", "<leader>gpd", function() goto_preview.goto_preview_definition() end, { silent = true, desc = "" })
-      vim.keymap.set("n", "<leader>gpt", function() goto_preview.goto_preview_type_definition() end, { silent = true, desc = "" })
-      vim.keymap.set("n", "<leader>gpi", function() goto_preview.goto_preview_implementation() end, { silent = true, desc = "" })
-      vim.keymap.set("n", "<leader>gP", function() goto_preview.close_all_win() end, { silent = true, desc = "" })
-      vim.keymap.set("n", "<leader>gpr", function() goto_preview.goto_preview_references() end, { silent = true, desc = "" })
-    end
-  },
 }
