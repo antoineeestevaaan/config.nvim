9fa3ab078c77c0e45489c07c10aba86a9a7ee1d2
amtoine <stevan.antoine@gmail.com>
2024-03-15 19:37:26 +0100

    refactor harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "theprimeagen/harpoon",
+    config = function()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+
+      vim.keymap.set("n", "<leader>ha", mark.add_file, { silent = true, desc = "[h]arpoon: [a]dd a file to the menu" })
+      vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu,
+        { silent = true, desc = "[h]arpoon: toggle menu [e]ntries" })
+      vim.keymap.set("n", "<leader>hh", function() ui.nav_file(1) end,
+        { silent = true, desc = "[h]arpoon: go to first item (left [h])" })
+      vim.keymap.set("n", "<leader>hj", function() ui.nav_file(2) end,
+        { silent = true, desc = "[h]arpoon: go to second item (down [j])" })
+      vim.keymap.set("n", "<leader>hk", function() ui.nav_file(3) end,
+        { silent = true, desc = "[h]arpoon: go to third item (up [k])" })
+      vim.keymap.set("n", "<leader>hl", function() ui.nav_file(4) end,
+        { silent = true, desc = "[h]arpoon: go to fourth item (right [l])" })
+    end
+  },
+}
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -23,26 +23,6 @@ return {
 
   -- motion in space and time
   {
-    "theprimeagen/harpoon",
-    config = function()
-      local mark = require("harpoon.mark")
-      local ui = require("harpoon.ui")
-
-      vim.keymap.set("n", "<leader>ha", mark.add_file, { silent = true, desc = "[h]arpoon: [a]dd a file to the menu" })
-      vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu,
-        { silent = true, desc = "[h]arpoon: toggle menu [e]ntries" })
-      vim.keymap.set("n", "<leader>hh", function() ui.nav_file(1) end,
-        { silent = true, desc = "[h]arpoon: go to first item (left [h])" })
-      vim.keymap.set("n", "<leader>hj", function() ui.nav_file(2) end,
-        { silent = true, desc = "[h]arpoon: go to second item (down [j])" })
-      vim.keymap.set("n", "<leader>hk", function() ui.nav_file(3) end,
-        { silent = true, desc = "[h]arpoon: go to third item (up [k])" })
-      vim.keymap.set("n", "<leader>hl", function() ui.nav_file(4) end,
-        { silent = true, desc = "[h]arpoon: go to fourth item (right [l])" })
-    end
-  },
-
-  {
     "mbbill/undotree",
     config = function()
       vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
