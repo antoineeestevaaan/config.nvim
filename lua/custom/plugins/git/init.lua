74509a5d29b02c6b349f68d5267b332bd35c3da8
amtoine <stevan.antoine@gmail.com>
2024-03-16 09:53:57 +0100

    move `vim-rhubarb` as dependency to `vim-fugitive`

--- lua/custom/plugins/git/init.lua
+++ lua/custom/plugins/git/init.lua
@@ -1,6 +1,7 @@
 return {
   {
     "tpope/vim-fugitive",
+    dependencies = { 'tpope/vim-rhubarb' },
     config = function()
       vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
     end
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,8 +2,6 @@ return {
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  'tpope/vim-rhubarb',
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 }
