39064898c3ffc0d71d2c398440b2739fa0840108
amtoine <stevan.antoine@gmail.com>
2024-04-24 22:17:23 +0200

    add some Typst plugins

---
+++ lua/custom/plugins/typst.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'kaarmu/typst.vim',
+    ft = 'typst',
+    lazy = false,
+  },
+
+  {
+    'chomosuke/typst-preview.nvim',
+    lazy = false, -- or ft = 'typst'
+    version = '0.1.*',
+    build = function() require 'typst-preview'.update() end,
+  }
+}
