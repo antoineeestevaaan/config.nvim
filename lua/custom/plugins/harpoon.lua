6dffabeb47b102e11445b04b57a5b5323efa31ac
amtoine <stevan.antoine@gmail.com>
2024-05-15 19:54:42 +0200

    refactor harpoon bindings

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,18 +9,20 @@ return {
         ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
       }
 
-      vim.keymap.set("n", "<leader>ha", mark.add_file,
-        { silent = true, desc = "[h]arpoon: [a]dd a file to the menu" })
-      vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu,
-        { silent = true, desc = "[h]arpoon: toggle menu [e]ntries" })
-      vim.keymap.set("n", "<leader>hh", function() ui.nav_file(1) end,
-        { silent = true, desc = "[h]arpoon: go to first item (left [h])" })
-      vim.keymap.set("n", "<leader>hj", function() ui.nav_file(2) end,
-        { silent = true, desc = "[h]arpoon: go to second item (down [j])" })
-      vim.keymap.set("n", "<leader>hk", function() ui.nav_file(3) end,
-        { silent = true, desc = "[h]arpoon: go to third item (up [k])" })
-      vim.keymap.set("n", "<leader>hl", function() ui.nav_file(4) end,
-        { silent = true, desc = "[h]arpoon: go to fourth item (right [l])" })
+      local nmap = function(keys, func, desc)
+        if desc then
+          desc = '[h]arpoon: ' .. desc
+        end
+
+        vim.keymap.set('n', keys, func, { silent = true, desc = desc })
+      end
+
+      nmap("<leader>ha", mark.add_file, "[h]arpoon: [a]dd a file to the menu")
+      nmap("<leader>he", ui.toggle_quick_menu, "[h]arpoon: toggle menu [e]ntries")
+      nmap("<leader>hh", function() ui.nav_file(1) end, "[h]arpoon: go to first item (left [h])")
+      nmap("<leader>hj", function() ui.nav_file(2) end, "[h]arpoon: go to second item (down [j])")
+      nmap("<leader>hk", function() ui.nav_file(3) end, "[h]arpoon: go to third item (up [k])")
+      nmap("<leader>hl", function() ui.nav_file(4) end, "[h]arpoon: go to fourth item (right [l])")
     end
   },
 }
