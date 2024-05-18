6c3501eda7d1ec553f21ea39373cff2353d2c9ee
amtoine <stevan.antoine@gmail.com>
2024-05-18 11:19:34 +0200

    better oil config

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,4 +2,21 @@ return {
   'stevearc/oil.nvim',
   opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local oil = require("oil")
+
+    oil.setup {
+      columns = { "icon", },
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<M-h>"] = "actions.select_split",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
+
+    vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
+    vim.keymap.set("n", "<leader>-", oil.toggle_float, { desc = "Open parent directory in floating mode" })
+  end,
 }
