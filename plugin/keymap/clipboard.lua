ddcbc6ead178208873d4b1f01e0878f2dff7951e
amtoine <stevan.antoine@gmail.com>
2024-06-01 13:02:25 +0200

    allow "paste before" on system clipboard

--- plugin/keymap/clipboard.lua
+++ plugin/keymap/clipboard.lua
@@ -1,3 +1,4 @@
 vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "paste without overwriting the register" })
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]], { desc = "paste without overwriting the register" })
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "yank to system clipboard only on demand" })
 vim.keymap.set({ "n", "v" }, "<leader>d", [["+d]], { desc = "cut to system clipboard only on demand" })
