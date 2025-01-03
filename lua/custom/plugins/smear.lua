a9c739b06124b83ebb9b6baa15c929003cc6046c
amtoine <stevan.antoine@gmail.com>
2025-01-03 17:29:44 +0100

    add Smear defaults

--- lua/custom/plugins/smear.lua
+++ lua/custom/plugins/smear.lua
@@ -1,5 +1,10 @@
 return {
   "sphamba/smear-cursor.nvim",
   enabled = true,
-  opts = {},
+  opts = {
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    legacy_computing_symbols_support = false,
+  },
 }
