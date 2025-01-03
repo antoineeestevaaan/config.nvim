29a3cc058da7d231bd65de0c2ad5742a2a723cf7
amtoine <stevan.antoine@gmail.com>
2025-01-03 17:35:53 +0100

    disable Smear for neighboring lines

--- lua/custom/plugins/smear.lua
+++ lua/custom/plugins/smear.lua
@@ -3,7 +3,7 @@ return {
   enabled = true,
   opts = {
     smear_between_buffers = true,
-    smear_between_neighbor_lines = true,
+    smear_between_neighbor_lines = false,
     scroll_buffer_space = true,
     legacy_computing_symbols_support = false,
   },
