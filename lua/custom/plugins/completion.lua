927c4912bdd36e1e847aeee3e86a300dffa41c23
amtoine <stevan.antoine@gmail.com>
2025-01-04 10:12:59 +0100

    use non-default Vim-like completion bindings

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -4,7 +4,28 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
     version = 'v0.*',
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = nil,
+
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide' },
+
+        ['<C-y>'] = {},
+        ['<C-l>'] = { 'select_and_accept' },
+
+        ['<C-p>'] = {},
+        ['<C-n>'] = {},
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+        ['<C-j>'] = { 'select_next', 'fallback' },
+
+        ['<C-b>'] = {},
+        ['<C-f>'] = {},
+        ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
