419cb8bc6ce57406d5842eb0ef23fe3b3a7d9920
a.stevan <antoine.stevan@isae-supaero.fr>
2025-01-28 16:13:08 +0100

    remove "word wrap" bindings

--- plugin/keymap/motions.lua
+++ plugin/keymap/motions.lua
@@ -8,7 +8,3 @@ vim.keymap.set("n", "<c-d>", "<c-d>zz", { silent = true })
 vim.keymap.set("n", "<c-u>", "<c-u>zz", { silent = true })
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
