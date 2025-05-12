4a86b6e5b3e76535291c3c1dd1237cfeb4401d2a
a.stevan <antoine.stevan@isae-supaero.fr>
2025-05-12 15:38:14 +0200

    add keymap (gd) to toggle Oil detailed columns

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,20 +4,34 @@ return {
   config = function()
     local oil = require("oil")
 
+    local detail = false
+    local DETAILED_COLUMNS = {
+      "icon",
+      { "permissions", highlight = "Floatborder" },
+      { "size",        highlight = "MatchParen" },
+      { "mtime",       highlight = "Whitespace", format = "%Y-%m-%d %T" },
+    }
+
     oil.setup {
       default_file_explorer = true,
       watch_for_changes = false,
-      columns = {
-        "icon",
-        { "permissions", highlight = "Floatborder" },
-        { "size",        highlight = "MatchParen" },
-        { "mtime",       highlight = "Whitespace", format = "%Y-%m-%d %T" },
-      },
+      columns = DETAILED_COLUMNS,
       skip_confirm_for_simple_edits = true,
       prompt_save_on_select_new_entry = true,
       keymaps = {
         ["<C-h>"] = false,
         ["<M-h>"] = "actions.select_split",
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              oil.set_columns(DETAILED_COLUMNS)
+            else
+              oil.set_columns({ "icon" })
+            end
+          end,
+        },
       },
       view_options = {
         show_hidden = true,
